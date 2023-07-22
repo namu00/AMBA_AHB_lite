c4bbe9456f6b1aee0647c36bbf3166a14cab770b
Kelvinlee501 <dlwhdxkr56@gmail.com>
2023-07-22 22:56:46 +0900

    update AHB bridge for HSIZE

--- Kelvinlee501/AMBA_BUS/AHB_SRAM_BRIDGE.v
+++ Kelvinlee501/AMBA_BUS/AHB_SRAM_BRIDGE.v
@@ -1,3 +1,9 @@
+//==============================================
+// V0.0.0 : initial ahb to sram bridge
+// V0.0.1 : Debug IDLE condition & FSM logic
+// V1.0.0 : Update spec to add HWORD,BYTE access (Liitle-Endian)
+//==============================================
+
 module AHB_SINGLE_PORT_SRAM_BRIDGE (
   //AMBA3 AHB-lite interface
   input I_HCLK,
@@ -40,7 +46,8 @@ module AHB_SINGLE_PORT_SRAM_BRIDGE (
   localparam TRANS_IDLE   = 2'b00;
   localparam TRANS_NONSEQ = 2'b10;
   
-  reg [9:0]  r_haddr;
+  reg [11:0]  r_haddr;
+  reg [2:0]   r_hsize;
   
   //FSM state
   reg [2:0] nstate;
@@ -49,11 +56,17 @@ module AHB_SINGLE_PORT_SRAM_BRIDGE (
   //wires
   wire w_ahb_trans_idle = ~|I_HTRANS;
   wire w_ahb_trans_nonseq = I_HTRANS[1]&~I_HTRANS[0];
+  wire w_non_word_cond = ~(r_hsize[2]|r_hsize[1]);
   
-  //clock ahb address
+  //capture ahb signals
   always @(posedge I_HCLK or negedge I_HRESETn) begin
-    if(!I_HRESETn) r_haddr <= 10'h0;
-    else if(I_HSEL & I_HREADY) r_haddr <= I_HADDR[11:2];
+    if(!I_HRESETn) r_haddr <= 12'h0;
+    else if(I_HSEL & I_HREADY) r_haddr <= I_HADDR[11:0];
+  end
+  
+  always @(posedge I_HCLK or negedge I_HRESETn) begin
+    if(!I_HRESETn) r_hsize <= 3'h0;
+    else if(I_HSEL & I_HREADY) r_hsize <= I_HSIZE;
   end
   
   //Hready output
@@ -63,7 +76,27 @@ module AHB_SINGLE_PORT_SRAM_BRIDGE (
   assign O_HRESP = 1'b0;
   
   //HRDATA
-  assign O_HRDATA = I_MRDATA;
+  always @(*) begin
+    if(w_non_word_cond) begin
+        if(r_size[0]) begin   //BYTE access
+            case(r_haddr[1:0])
+                2'b00   : O_HRDATA = {24'h00_0000,I_MRDATA[7:0]};
+                2'b01   : O_HRDATA = {16'h00_0000,I_MRDATA[15:8],8'h00};
+                2'b10   : O_HRDATA = {8'h00_0000,I_MRDATA[23:16],16'h0000};
+                2'b11   : O_HRDATA = {I_MRDATA[31:24],24'h00_0000};
+                default : O_HRDATA = {24'h00_0000,I_MRDATA[7:0]};
+            endcase
+        end
+        else begin            //HWORD access
+            case(r_haddr[1:0])
+                2'b00   : O_HRDATA = {16'h0000,I_MRDATA[15:0]};
+                2'b10   : O_HRDATA = {I_MRDATA[31:16],16'h0000};
+                default : O_HRDATA = {16'h0000,I_MRDATA[15:0]};
+            endcase
+        end
+    end
+    else O_HRDATA = I_MRDATA; //WORD access
+  end
   
   //simple control FSM
   always @(posedge I_HCLK or negedge I_HRESETn) begin
@@ -101,12 +134,33 @@ module AHB_SINGLE_PORT_SRAM_BRIDGE (
   end
   
   //ADDR
-  assign O_MADDR = (I_HREADY) ? I_HADDR[11:2] : r_haddr;
+  assign O_MADDR = (I_HREADY) ? I_HADDR[11:2] : r_haddr[11:2];
   
   //WREN
   assign O_MWREN = pstate[1];
   
   //DATA
-  assign O_MWDATA = I_HWDATA;
+  always @(*) begin
+    if(w_non_word_cond) begin
+        if(r_size[0]) begin   //BYTE access
+            case(r_haddr[1:0])
+                2'b00   : O_MWDATA = {I_MRDATA[31:8],I_HWDATA[7:0]};
+                2'b01   : O_MWDATA = {I_MRDATA[31:16],I_HWDATA[15:8],I_MRDATA[7:0]};
+                2'b10   : O_MWDATA = {I_MRDATA[31:24],I_HWDATA[23:16],I_MRDATA[15:0]};
+                2'b11   : O_MWDATA = {I_HWDATA[31:24],I_MRDATA[23:0]};
+                default : O_MWDATA = {I_MRDATA[31:8],I_HWDATA[7:0]};
+            endcase
+        end
+        else begin            //HWORD access
+            case(r_haddr[1:0])
+                2'b00   : O_MWDATA = {I_MRDATA[31:16],I_HWDATA[15:0]};
+                2'b10   : O_MWDATA = {I_HWDATA[31:16],I_MRDATA[15:0]};
+                default : O_MWDATA = {I_MRDATA[31:16],I_HWDATA[15:0]};
+            endcase
+        end
+    end
+    else O_MWDATA = I_HWDATA; //WORD access
+  end
+
   
 endmodule
